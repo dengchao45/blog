becbdf078e863972300c8430234df426c9cff117
Issue <zhushisong@skio.com>
2017-04-07 13:35:22 +0800

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    # config.time_zone = :local
+    config.i18n.default_locale = :zh
+    config.time_zone = 'Beijing'
   end
 end
